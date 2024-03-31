552b175498ecbdbfdcfa5882494ce9390eef9c50
s599khan <s599khan@uwaterloo.ca>
2024-03-30 21:15:45 -0400

    avoid global uses of namespace std

--- tile.h
+++ tile.h
@@ -4,17 +4,16 @@
 #include <string>
 #include "subject.h"
 #include "player.h"
-using namespace std;
 
 class Tile : public Subject {
-    string name;  // Name of the tile
+    std::string name;  // Name of the tile
     int location; // Position of tile on the board
 
   public:
-    Tile(string name, int loc); // Constructor for Tile
+    Tile(std::string name, int loc); // Constructor for Tile
 
     int getLocation() const;  // returns name field of Tile
-    string getName() const;   // returns location field of Tile
+    std::string getName() const;   // returns location field of Tile
     
     virtual void performAction(Player &p) = 0; // pure virtual, no implimentation, for subclasses
     virtual ~Tile() = default; // Default destructor
