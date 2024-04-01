db2b47185690553e50ba2a3fac6075ed1760a1bf
s599khan <s599khan@uwaterloo.ca>
2024-03-31 22:04:01 -0400

    removed banktrupcy related fields and methods from school

--- player.h
+++ player.h
@@ -11,36 +11,34 @@ private:
     char piece;
     std::string name;
     int wallet;
+    int boardSize;
     int position;
-    bool bankrupt;
     bool visitingTims;  // true when visting false when in line
     int timsLine; // number of turns in line
     int timsCups;
     School& school;
 
 public:
-    Player(char piece, const std::string& name, int wallet, int position = 0, bool bankrupt = false, bool visitingTims = true, int timsLine = 0, 
+    Player(char piece, const std::string& name, int wallet, School& school, int boardSize, int position = 0, bool bankrupt = false, bool visitingTims = true, int timsLine = 0, 
            int timsCups = 0);
-    int countGym();
-    int countRest();
-    void printAssets();
-    void isBankrupt();
-    int getWallet();
+    int countGym() const; // counts number of gym's owned
+    int countRes() const; // counts number of residence's owned
+    void printAssets() const;
+    int getWallet() const;
     void setWallet(const int amount);
     void addWallet(const int amount); // adds amount to wallet field
-    char getPiece();
+    char getPiece() const;
     void setPosition(const int pos);
     int getPosition() const; // Returns position field
     int movePosition(const int n); // moves player forward or backwards (if n < 0) by n tiles
-    int getTimsLine();
+    int getTimsLine() const;
     void setTimsLine(const int pos);
-    int incTimsLine() const; // increments timsLine by 1
+    void incTimsLine(); // increments timsLine by 1
     bool isVisitingTims() const; // returns visitingTims field;
     void toggleVisiting(); // Toggles visitingTims to opposite of what it was
-    void getTimsCups() const; // returns timsCups field
+    int getTimsCups() const; // returns timsCups field
     void addTimsCups(const int n); // Adds n to timsCups field
-
-    std::string getName();
+    std::string getName() const;
 };
 
 #endif // PLAYER_H
