366a05bc667f35689a00f49ef06553064fc201df
s599khan <s599khan@uwaterloo.ca>
2024-04-01 06:43:44 -0400

    removed decorator related details from main branch

--- ownableproperty.cc
+++ ownableproperty.cc
@@ -17,13 +17,8 @@ int OwnableProperty::getFee() const { return config.getFee(0); }
 
 string OwnableProperty::getGroup() const { return config.getGroup(); }
 
-
 void OwnableProperty::toggleMortgage() { mortgaged = !(mortgaged); }
 
 void OwnableProperty::toggleOwnership() { owned = !(owned); }
-
-int OwnableProperty::getDepth() const { return 0; }
-
-bool OwnableProperty::isPropertyEnhancement() const { return false; }
 
 const PropertyConfig& OwnableProperty::getConfig() const { return config; }
