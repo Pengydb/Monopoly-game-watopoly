29147fb497fb5e39cd342df42b9ad090291bedb3
mjdemarc <mjdemarc@uwaterloo.ca>
2024-04-01 15:44:07 -0400

    Changed method names for DCTimsCards to DCTimsCups

--- school.h
+++ school.h
@@ -13,15 +13,15 @@ class School: public Observer {
 private:
     std::map<std::weak_ptr<OwnableProperty>, std::weak_ptr<Player>, std::owner_less<std::weak_ptr<OwnableProperty>>> propertyOwnership;
     std::map<std::string, std::shared_ptr<PropertyConfig>> propertyConfigs;
-    int DCTimsCards; // total number of DCTims Cards that are owned by players
+    int DCTimsCups; // total number of DCTims Cups that are owned by players
 
 public:
     School(const std::vector<std::shared_ptr<Player>>& players, const std::vector<std::shared_ptr<OwnableProperty>>& properties, int cards);
     ~School();
 
     void notify(Subject& s);
-    int getDCTimsCards() const; // returns DCTimsCards field
-    void addDCTimsCards(int n); // adds n to DCTimsCards field
+    int getDCTimsCups() const; // returns DCTimsCups field
+    void addDCTimsCups(int n); // adds n to DCTimsCups field
 
     void holdAuction(const std::weak_ptr<OwnableProperty>& property);
 
