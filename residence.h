c1e3607cbefdec7d5c438d4ae6e09e90e3992561
mjdemarc <mjdemarc@uwaterloo.ca>
2024-04-02 22:30:53 -0400

    fixed typo

--- residence.h
+++ residence.h
@@ -6,6 +6,6 @@
 class Residence : public OwnableProperty {
   public:
     Residence(std::string name, int loc, PropertyConfig& config, bool owned, bool mortgaged);
-    void performAction(Player &p, Bank &s) override;
+    void performAction(Player &p, Bank &b) override;
 };
 #endif // _RESIDENCE_H
