ee2bf573eeb78a441c10716365bd2431cac3faa5
s599khan <s599khan@uwaterloo.ca>
2024-04-05 12:51:02 -0400

    updated gitingore to ignore compiler generated files

--- residence.cc
+++ residence.cc
@@ -16,6 +16,12 @@ void Residence::performAction(Player &p, Bank &b)
     {
         std::string owner = b.getPropertyOwner(this->getName());
         std::cout << "This property is owned by " << owner << std::endl;
+
+        if (p.getName() == owner) {
+            std::cout << p.getName() <<  ", you do not need to pay tuition as you are the owner." << std::endl;
+            return;
+        }
+
         int fee = config->getFee(std::max(p.getRes() - 1, 0));
 
         std::cout << "You are being charged with a fee of $" << fee << std::endl;
